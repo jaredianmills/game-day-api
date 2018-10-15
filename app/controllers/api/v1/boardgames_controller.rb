3be0f9efe8db8eb40f2855a0f177054cfeb741ed
jaredianmills <jaredianmills@gmail.com>
2018-10-15 16:50:29 -0400

    parsed proper boardgame title out of xml with nokogiri

--- app/controllers/api/v1/boardgames_controller.rb
+++ app/controllers/api/v1/boardgames_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::BoardgamesController < ApplicationController
 
   def search
+    response = []
     url = "https://www.boardgamegeek.com/xmlapi/search?search=#{params[:search_term]}"
     request = RestClient.get(url)
-    response = Hash.from_xml(request)
+    boardgames = Hash.from_xml(request)
+    boardgames['boardgames']['boardgame'].each do |boardgame|
+      boardgame_url = "https://www.boardgamegeek.com/xmlapi/boardgame/#{boardgame['objectid']}"
+      boardgame_request = RestClient.get(boardgame_url)
+      boardgame_details = Hash.from_xml(boardgame_request)
+      title = Nokogiri::XML(boardgame_request).xpath('//name').find{|name| name.attributes['primary']}.text
+      boardgame_details[:title] = title
+      response << boardgame_details
+    end
     render json: response
   end
 
