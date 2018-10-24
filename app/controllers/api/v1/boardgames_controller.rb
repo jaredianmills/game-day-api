442f0c789b05f5da9eb375d0cc41bcf1e27a78b9
jaredianmills <jaredianmills@gmail.com>
2018-10-24 17:28:57 -0400

    successfully parsed best_at player count for games

--- app/controllers/api/v1/boardgames_controller.rb
+++ app/controllers/api/v1/boardgames_controller.rb
@@ -4,14 +4,14 @@ class Api::V1::BoardgamesController < ApplicationController
     boardgames_response = []
     url = "https://www.boardgamegeek.com/xmlapi/search?search=#{params[:search_term]}"
     request = RestClient.get(url)
-    boardgames = Hash.from_xml(request)
-    if boardgames['boardgames']['boardgame']
-      boardgames['boardgames']['boardgame'].each do |boardgame|
+    boardgames = Hash.from_xml(request)['boardgames']['boardgame']
+    if boardgames
+      boardgames.each do |boardgame|
         boardgames_response << find_by_id(boardgame)
       end
       render json: boardgames_response
     else
-      render json: {error: 'Could not find that game'}
+      render json: {error: "We couldn't find any matching games"}
     end
   end
 
@@ -26,8 +26,22 @@ class Api::V1::BoardgamesController < ApplicationController
     boardgame_request = RestClient.get(boardgame_url)
     boardgame_details = Hash.from_xml(boardgame_request)
     title = Nokogiri::XML(boardgame_request).xpath('//name').find{|name| name.attributes['primary']}.text
-    boardgame_details['boardgames']['boardgame'][:title] = title
-    boardgame_details['boardgames']['boardgame']
+    boardgame_details = boardgame_details['boardgames']['boardgame']
+    boardgame_details[:title] = title
+    suggested_num_players = boardgame_details['poll'].find {|poll| poll['name'] = 'suggested_numplayers'}['results']
+    highest_num_votes = 0
+    best_at = nil
+
+    suggested_num_players.each do |result|
+      num_players = result['numplayers']
+      votes = result["result"].find {|poll_result| poll_result["value"] == "Best"}["numvotes"]
+      if votes.to_i > highest_num_votes
+        highest_num_votes = votes.to_i
+        best_at = num_players
+      end
+    end
+    boardgame_details["best_at"] = best_at
+    boardgame_details
   end
 
   def index
