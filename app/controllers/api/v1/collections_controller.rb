47385ecd7288d8ef6eb4471a4c1c5db66aa7aa34
jaredianmills <jaredianmills@gmail.com>
2018-11-20 16:29:04 -0500

    add handling around bgg api giving a message upon initial request for user collection

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -4,13 +4,21 @@ class Api::V1::CollectionsController < ApplicationController
     url = "https://www.boardgamegeek.com/xmlapi/collection/#{params[:search_term]}"
     request = RestClient.get(url)
     collection = Hash.from_xml(request)
-    filtered_collection = collection['items']['item'].select {|game| game['status']['own'] === '1'}
 
-    if filtered_collection
-      render json: filtered_collection
-    else
-      self.search
+    while collection['message'] && collection['message'].include?('Please try again later')
+      sleep 1
+      request = RestClient.get(url)
+      collection = Hash.from_xml(request)
     end
+
+    if collection['errors']
+      render json: collection['errors']
+    else
+      filtered_collection = collection['items']['item'].select {|game| game['status']['own'] === '1'}
+      render json: filtered_collection
+    end
+
   end
+
 
 end
