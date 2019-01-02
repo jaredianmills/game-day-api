2453689b65088c68e84d5319e4d9e60aac3649bb
jaredianmills <jaredianmills@gmail.com>
2019-01-02 14:02:19 -0500

    refactor to allow for multiple player counts for the best_at count

--- app/controllers/api/v1/boardgames_controller.rb
+++ app/controllers/api/v1/boardgames_controller.rb
@@ -29,24 +29,53 @@ class Api::V1::BoardgamesController < ApplicationController
     boardgame_details = boardgame_details['boardgames']['boardgame']
     boardgame_details[:title] = title
     suggested_num_players = boardgame_details['poll'].find {|poll| poll['name'] = 'suggested_numplayers'}['results']
-    boardgame_details["best_at"] = determine_best_at_count(suggested_num_players)
+    boardgame_details[:best_at] = get_best_at(suggested_num_players)
     boardgame_details
   end
 
-  def determine_best_at_count(suggested_num_players)
+  # def determine_best_at_count(suggested_num_players)
+  #   highest_num_votes = 0
+  #   best_at = nil
+  #   if suggested_num_players.class == Array
+  #     suggested_num_players.each do |result|
+  #       num_players = result['numplayers']
+  #       votes = result["result"].find {|poll_result| poll_result["value"] == "Best"}['numvotes'].to_i
+  #       if votes > highest_num_votes
+  #         highest_num_votes = votes
+  #         best_at = num_players
+  #       end
+  #     end
+  #   end
+  #   best_at
+  # end
+
+  def get_best_at(suggested_num_players)
     highest_num_votes = 0
     best_at = nil
     if suggested_num_players.class == Array
+      best_at_hash = {}
+      total_num_votes = 0
       suggested_num_players.each do |result|
         num_players = result['numplayers']
         votes = result["result"].find {|poll_result| poll_result["value"] == "Best"}['numvotes'].to_i
-        if votes > highest_num_votes
-          highest_num_votes = votes
-          best_at = num_players
-        end
+        best_at_hash[num_players] = votes
+        total_num_votes += votes
       end
+      best_at = determine_best_at_count(best_at_hash)
     end
     best_at
+  end
+
+  def determine_best_at_count(best_at_hash)
+    highest_suggested_player_count = best_at_hash.max_by{|k, v| v}
+    best_at = [highest_suggested_player_count[0]]
+
+    best_at_hash.each do |player_count, num_votes|
+      if highest_suggested_player_count[1] / num_votes.to_f <= 1.2 && highest_suggested_player_count[0] != player_count
+        best_at.push(player_count)
+      end
+    end
+    best_at.sort
   end
 
   def index
